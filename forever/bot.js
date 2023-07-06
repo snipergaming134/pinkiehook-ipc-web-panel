3b9bafefec55e00438d32ea5f4c5c0ad83342355
rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-07-05 20:03:27 -0400

    rat.conf

--- forever/bot.js
+++ forever/bot.js
@@ -10,9 +10,9 @@ const { Tail } = require("tail");
 const accounts = require('./acc.js');
 const config = require('./config');
 
-const LAUNCH_OPTIONS_STEAM = 'firejail --dns=1.1.1.1 %NETWORK% --noprofile --private="%HOME%" --name=%JAILNAME% --env=PULSE_SERVER="unix:/tmp/pulse.sock" --env=DISPLAY=%DISPLAY% --env=LD_PRELOAD=%LD_PRELOAD% %STEAM% -silent -noreactlogin  -login %LOGIN% %PASSWORD% -cef-disable-gpu -nominidumps -nobreakpad -no-browser -nofriendsui -vgui -noasync -nofasthtml -noshaders -oldtraymenu -single_core'
+const LAUNCH_OPTIONS_STEAM = 'firejail --dns=1.1.1.1 %NETWORK% --noprofile --blacklist=%STEAMWEBHELPERPATH% --private="%HOME%" --name=%JAILNAME% --env=PULSE_SERVER="unix:/tmp/pulse.sock" --env=DISPLAY=%DISPLAY% --env=LD_PRELOAD=%LD_PRELOAD% %STEAM% -silent -noreactlogin  -login %LOGIN% %PASSWORD% -cef-disable-gpu -nominidumps -nobreakpad -no-browser -vrdisable -nocrashmonitor -nofriendsui -vgui -noasync -nofasthtml -noshaders -oldtraymenu -single_core'
 const LAUNCH_OPTIONS_STEAM_RESET = 'firejail --net=none --noprofile --private="%HOME%" %STEAM% --reset'
-const LAUNCH_OPTIONS_GAME = 'firejail --join=%JAILNAME% bash -c \'cd ~/$GAMEPATH && %REPLACE_RUNTIME% LD_PRELOAD=%LD_PRELOAD% DISPLAY=%DISPLAY% PULSE_SERVER="unix:/tmp/pulse.sock" ./hl2_linux -game tf -silent -sw -w 640 -h 200 -novid -nojoy -noshaderapi -nomouse -nomessagebox -nominidumps -nohltv -nobreakpad -reuse -noquicktime -precachefontchars -particles 512 -snoforceformat -softparticlesdefaultoff -wavonly -forcenovsync -threads 1\''
+const LAUNCH_OPTIONS_GAME = 'firejail --join=%JAILNAME% bash -c \'cd ~/$GAMEPATH && %REPLACE_RUNTIME% LD_PRELOAD=%LD_PRELOAD% DISPLAY=%DISPLAY% PULSE_SERVER="unix:/tmp/pulse.sock" ./hl2_linux -game tf -silent -sw -w 640 -h 200 -novid -nojoy -noshaderapi -nomouse -nomessagebox -nominidumps -nohltv -nobreakpad -nosrgb -nostartupsound -noquicktime -no_texture_stream -nouserclip -mat_softwaretl -precachefontchars -limitvsconst -reuse -noquicktime -precachefontchars -particles 512 -snoforceformat -softparticlesdefaultoff -wavonly -forcenovsync -threads 1 +mat_aaquality 0 +mat_antialias 0\''
 const LAUNCH_OPTIONS_GAME_NATIVE = LAUNCH_OPTIONS_GAME.replace("%REPLACE_RUNTIME%", 'LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./bin"');
 const LAUNCH_OPTIONS_GAME_RUNTIME = LAUNCH_OPTIONS_GAME.replace("%REPLACE_RUNTIME%", 'LD_LIBRARY_PATH="$(~/"%STEAM_RUNTIME%" printenv LD_LIBRARY_PATH):./bin"');
 
@@ -193,6 +193,8 @@ class Bot extends EventEmitter {
             .replace("%LOGIN%", self.account.login)
             // Password
             .replace("%PASSWORD%", self.account.password)
+            // ok
+            .replace("%STEAMWEBHELPERPATH%", path.relative(self.home, path.join(self.steamPath, "/ubuntu12_64/steamwebhelper.sh")))
             // Name of the firejail jail
             .replace("%JAILNAME%", self.name)
             // LD_PRELOAD, "just disable vac"
