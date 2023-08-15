45e1856fb7c154f5a28c89a1ede6b01329e98b86
Rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-08-15 16:34:13 -0400

    test

--- run.sh
+++ run.sh
@@ -7,4 +7,10 @@ if [ $EUID != 0 ]; then
 	exit
 fi
 
+if [[ $steam_command == "steam-native" ]]; then
+  library_path="$LD_LIBRARY_PATH:./bin"
+else
+  library_path="$(/home/rosnegaruda/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh printenv LD_LIBRARY_PATH):./bin"
+fi
+
 $(which node || which nodejs) app >$LOG
